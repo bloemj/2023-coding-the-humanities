3f39ef9d3bac69a472855beb2895f463c22cb3d5
Leon van Wissen <leon@vwissen.nl>
2021-02-01 15:10:01 +0100

    syntax

--- notebooks/example_module.py
+++ notebooks/example_module.py
@@ -1,5 +1,11 @@
-def sum_function(l):
-	result = 0
-	for i in l:
-		result += i
-	return result
+def sum_function(values):
+    result = 0
+    for i in values:
+        result += i
+    return result
+
+
+if __name__ == "__main__":
+    new_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    print(sum_function(new_values))
